801c250d207a5b58caa3d7e6f870a23d8bd7f5e9
JulenJR <julenjr@hotmail.com>
2023-03-13 12:37:48 +0100

    adding inserts

--- js/Sprint2/optica/empleatinsert.sql
+++
@@ -1,7 +0,0 @@
-/*
--- Query: 
--- Date: 2023-03-13 12:22
-*/
-INSERT INTO `` (`ID_empelat`,`nom`) VALUES (1,'nom1');
-INSERT INTO `` (`ID_empelat`,`nom`) VALUES (2,'nom2');
-INSERT INTO `` (`ID_empelat`,`nom`) VALUES (3,'nom3');
