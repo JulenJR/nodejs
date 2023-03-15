cb2d3a59831b865077306fa02d7a2da378b6db7a
JulenJR <julenjr@hotmail.com>
2023-03-15 12:24:45 +0100

    date solucionat #2

--- js/Sprint2/optica/query_vendes_empleat.sql
+++ js/Sprint2/optica/query_vendes_empleat.sql
@@ -1,4 +1,4 @@
 -- Llista les diferents ulleres que ha venut un empleat durant un any.
 SELECT * FROM vendes 
-WHERE YEAR(data_venda) = 0000 
-AND Empleat_ID_empelat = 1;
+WHERE YEAR(data_venda) = 2003
+AND Empleat_ID_empelat = 2;
