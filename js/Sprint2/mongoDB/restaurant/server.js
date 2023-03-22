05868e592f13924974c88158ae9384e31de56231
JulenJR <julenjr@hotmail.com>
2023-03-22 16:45:54 +0100

    server testing

---
+++ js/Sprint2/mongoDB/restaurant/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const uri = 'mongodb+srv://julenjr:JuLeNunodostrescuatro@mycluster.2tyy3ze.mongodb.net/?retryWrites=true&w=majority';
+
+async function conect(){
+    try{
+        mongoose.connect(uri);
+        console.log("Connected to MongoDB");
+    }catch (err) {
+        console.error(err);
+    }
+}
+
+conect();
+
+app.listen(8000, () => {
+  console.log("Server started on port 8000");
+});
+
+
