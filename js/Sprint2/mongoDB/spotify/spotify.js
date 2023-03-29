8e9fc25b3fedde3ad8ca43d357f30ba9ada963e2
JulenJR <julenjr@hotmail.com>
2023-03-29 09:24:32 +0200

    Update spotify.js

--- js/Sprint2/mongoDB/spotify/spotify.js
+++ js/Sprint2/mongoDB/spotify/spotify.js
@@ -30,94 +30,94 @@ db.suscripcioPayPal.insertMany(
 db.suscripcioTargetaDeCredit.drop();
 db.suscripcioTargetaDeCredit.insertMany(
   [{
-  "_id": {
-    "$oid": "6422a3a907c7d1e020f4a072"
-  },
-  "caducitat": {
-    "$date": {
-      "$numberLong": "292091180400000"
+    "_id": {
+      "$oid": "6422a3a907c7d1e020f4a072"
+    },
+    "caducitat": {
+      "$date": {
+        "$numberLong": "292091180400000"
+      }
+    },
+    "numeroTargeta": "481487526457832",
+    "pin": 2525,
+    "usuari_id": {
+      "$oid": "6422a40f07c7d1e020f4a073"
     }
-  },
-  "numeroTargeta": "481487526457832",
-  "pin": 2525,
-  "usuari_id": {
-    "$oid": "6422a40f07c7d1e020f4a073"
-  }
-}]
+  }]
 );
 
 db.usuari.drop();
 db.usuari.insertMany(
-  [{
-  "_id": {
-    "$oid": "642294aa07c7d1e020f4a061"
-  },
-  "codiPostal": "022510",
-  "correuElectronic": "user1@bmail.ko",
-  "dataNaixement": {
-    "$date": {
-      "$numberLong": "3421657206000000"
-    }
-  },
-  "isPremium": true,
-  "nomUsuari": "usuari1",
-  "pais": "Mozambique",
-  "password": "user1password",
-  "sexe": "H",
-  "suscripcio": {
-    "dataIniciSuscripcio": {
+    [{
+    "_id": {
+      "$oid": "642294aa07c7d1e020f4a061"
+    },
+    "codiPostal": "022510",
+    "correuElectronic": "user1@bmail.ko",
+    "dataNaixement": {
       "$date": {
-        "$numberLong": "4381398774000000"
+        "$numberLong": "3421657206000000"
       }
     },
-    "dataRenovacio": {
-      "$date": {
-        "$numberLong": "3750291270000000"
-      }
+    "isPremium": true,
+    "nomUsuari": "usuari1",
+    "pais": "Mozambique",
+    "password": "user1password",
+    "sexe": "H",
+    "suscripcio": {
+      "dataIniciSuscripcio": {
+        "$date": {
+          "$numberLong": "4381398774000000"
+        }
+      },
+      "dataRenovacio": {
+        "$date": {
+          "$numberLong": "3750291270000000"
+        }
+      },
+      "metodepagaments": "PayPal"
     },
-    "metodepagaments": "PayPal"
-  },
-  "pagaments": [
-    {
-      "pagament_id": {
-        "$oid": "64229fd207c7d1e020f4a069"
-      },
-      "dataPagament": {
-        "$date": {
-          "$numberLong": "7843512006000000"
-        }
-      },
-      "total": 12.99
-    }
-  ],
-  "playlists": [
-    {
-      "playlist_id": {
-        "$oid": "6422a43b07c7d1e020f4a075"
-      },
-      "nomPlaylist": "playlist1usuari1",
-      "dataCreacioPlaylist": {
-        "$date": {
-          "$numberLong": "8152770092400000"
-        }
-      },
-      "cançons": [
-        {
-          "canço_id": {
-            "$oid": "6422a5b707c7d1e020f4a079"
-          },
-          "dataInsert": {
-            "$date": {
-              "$numberLong": "266719388400000"
-            }
-          },
-          "usuari_inserta_canço": {
-            "$oid": "642294aa07c7d1e020f4a061"
+    "pagaments": [
+      {
+        "pagament_id": {
+          "$oid": "64229fd207c7d1e020f4a069"
+        },
+        "dataPagament": {
+          "$date": {
+            "$numberLong": "7843512006000000"
           }
-        }
-      ],
-      "statusPlaylist": "activa"
-    }
-  ]
-}]
+        },
+        "total": 12.99
+      }
+    ],
+    "playlists": [
+      {
+        "playlist_id": {
+          "$oid": "6422a43b07c7d1e020f4a075"
+        },
+        "nomPlaylist": "playlist1usuari1",
+        "dataCreacioPlaylist": {
+          "$date": {
+            "$numberLong": "8152770092400000"
+          }
+        },
+        "cançons": [
+          {
+            "canço_id": {
+              "$oid": "6422a5b707c7d1e020f4a079"
+            },
+            "dataInsert": {
+              "$date": {
+                "$numberLong": "266719388400000"
+              }
+            },
+            "usuari_inserta_canço": {
+              "$oid": "642294aa07c7d1e020f4a061"
+            }
+          }
+        ],
+        "statusPlaylist": "activa"
+      }
+    ]
+  }]
 );
