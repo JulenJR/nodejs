9c72c422606d83d381bc25bf8a08e19aeedb573e
JulenJR <julenjr@hotmail.com>
2023-03-30 10:33:12 +0200

    script to fullfill a database without using playground

--- js/Sprint2/mongoDB/youtube/youtubeInit.js
+++ js/Sprint2/mongoDB/youtube/youtubeInit.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require('mongodb');
+
 const MongoClient = require('mongodb').MongoClient;
 
 const uri = 'mongodb+srv://julenjr:JuLeNunodostrescuatro@mycluster.2tyy3ze.mongodb.net/?retryWrites=true&w=majority';
@@ -5,14 +7,8 @@ const dbName = 'youtube';
 
 const usuari = [
     {
-        "_id": {
-          "$oid": "64214b41111bbf073dd05507"
-        },
-        "dataNaixement": {
-          "$date": {
-            "$numberLong": "5940091234800000"
-          }
-        },
+        _id: new ObjectId("64214b41111bbf073dd05507"),
+        "dataNaixement": new Date("5940091234800000"),
         "email": "usuari1@gmail.com",
         "nomUsuari": "usuari1",
         "password": "contraseñaUser1",
@@ -21,99 +17,55 @@ const usuari = [
         "sexe": "M",
         "videos_upload_byUser": [
           {
-            "video_id": {
-              "$oid": "64214c78111bbf073dd05509"
-            }
+            "video_id": new ObjectId("64214c78111bbf073dd05509")
           }
         ],
         "canal": {
-          "canal_id": {
-            "$oid": "64215038111bbf073dd05511"
-          },
-          "nomCanal": "canaluser1",
+          canal_id : new ObjectId("64215038111bbf073dd05511"),
+          nomCanal: "canaluser1",
           "descripcio": "descripcio del canal creat per uruasi1",
-          "dataCreacioCanal": {
-            "$date": {
-              "$numberLong": "5308826050800000"
-            }
+          "dataCreacioCanal":new Date("5308826050800000")
           },
-          "suscriptors": 100000001
-        },
+          "suscriptors": 100000001,
         "dislikes": [
           {
-            "video_id": {
-              "$oid": "6421518a111bbf073dd05515"
-            },
-            "dataDislike": {
-              "$date": {
-                "$numberLong": "3763198134000000"
-              }
-            }
+            "video_id": new ObjectId("6421518a111bbf073dd05515"),
+            "dataDislike": new Date("3763198134000000"),
           }
         ],
         "likes": [
           {
-            "video_id": {
-              "$oid": "6421513c111bbf073dd05514"
-            },
-            "dataLike": {
-              "$date": {
-                "$numberLong": "3444441231600000"
-              }
-            }
+            "video_id": new ObjectId("6421513c111bbf073dd05514"),
+            "dataLike": new Date("3444441231600000"),
           }
         ],
         "canalsSuscrit": [
           {
-            "canal_id": {
-              "$oid": "64215212111bbf073dd05518"
-            }
+            "canal_id": new ObjectId("64215212111bbf073dd05518"),
           }
         ],
         "playlists": [
           {
-            "playlist_id": {
-              "$oid": "64215283111bbf073dd05519"
-            },
+            "playlist_id": new ObjectId("64215283111bbf073dd05519"),
             "nomPlaylist": "playlist1",
-            "dataCreacioPlaylist": {
-              "$date": {
-                "$numberLong": "7840387868400000"
-              }
-            },
+            "dataCreacioPlaylist": new ObjectId("784038786840000000000000"),
             "status": "publica"
           }
         ],
         "comentarisDislike": [
           {
-            "video_id": {
-              "$oid": "64215820111bbf073dd05521"
-            },
+            "video_id": new ObjectId("64215820111bbf073dd05521"),
             "comentari": "comentari dislike1 by user1 at video (x)",
-            "data": {
-              "$date": {
-                "$numberLong": "4684313977200000"
-              }
-            },
-            "comentari_id": {
-              "$oid": "6422bbf207c7d1e020f4a07e"
-            }
+            "data": new Date("4684313977200000"),
+            "comentari_id": new ObjectId("6422bbf207c7d1e020f4a07e"),
           }
         ],
         "comentarisLike": [
           {
-            "video_id": {
-              "$oid": "642157c0111bbf073dd05520"
-            },
+            "video_id": new ObjectId("642157c0111bbf073dd05520"),
             "comentari": "coenmtari c1 by user1 at video (x)",
-            "data": {
-              "$date": {
-                "$numberLong": "4703658332400000"
-              }
-            },
-            "comentari_id": {
-              "$oid": "6422bbfc07c7d1e020f4a07f"
-            }
+            "data": new Date("4703658332400000"),
+            "comentari_id": new ObjectId("6422bbfc07c7d1e020f4a07f"),
           }
         ]
       }
@@ -121,9 +73,7 @@ const usuari = [
 
 const video = [
     {
-        "_id": {
-          "$oid": "64214c78111bbf073dd05509"
-        },
+        "_id": new ObjectId("64214c78111bbf073dd05409"),
         "descripcio": "descripcioVideo1",
         "dislikes": 3313,
         "durada": 13345,
@@ -136,76 +86,56 @@ const video = [
         "status": "privat",
         "etiquetas": [
           {
-            "etiqueta_id": {
-              "$oid": "64214e5c111bbf073dd0550c"
-            },
+            "etiqueta_id": new ObjectId("64214e5c111bbf073dd0550c"),
             "nomEtiqueta": "etiqueta1"
           }
         ],
-        "dataPublicacion": {
-          "$date": {
-            "$numberLong": "4388025740400000"
-          }
-        },
+        "dataPublicacion": new Date("4388025740400000"),
         "comentaris": [
           {
-            "comentari_id": {
-              "$oid": "6421562a111bbf073dd0551b"
-            },
-            "dataComentari": {
-              "$date": {
-                "$numberLong": "4678412770800000"
-              }
-            },
-            "usuari_id": {
-              "$oid": "64214b41111bbf073dd05507"
-            }
+            "comentari_id": new ObjectId("6421562a111bbf073dd0551b"),
+            "dataComentari": new Date("4678412770800000"),
+            "usuari_id": new ObjectId("64214b41111bbf074dd05507"),
           }
         ]
       }
 ];
 
+// Connect to the MongoDB server
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-client.connect(err => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  
+client.connect()
+  .then(() => {
     console.log('Connected successfully to server');
-  
+
     // Get the reference to the database
     const db = client.db(dbName);
 
-    // Clear the collection before insert values
-    db.collection('usuari').drop();
-  
-    // Insert "usuari" collection
-    db.collection('usuari').insertMany(usuari, (err, result) => {
-      if (err) {
+    // Insert usuari collection
+    return db.collection('usuari').insertMany(usuari)
+      .then(result => {
+        console.log(`${result.insertedCount} documents were inserted into the 'usuari' collection`);
+      })
+      .catch(err => {
         console.error(err);
-        return;
-      }
-  
-      console.log(`${result.insertedCount} documents were inserted into the 'usuari' collection`);
-    });
+      });
+  })
+  .then(() => {
+    // Insert video collection
+    const db = client.db(dbName);
 
-    // Clear the collection before insert values
-    db.collection('video').drop();
-  
-    // Insert "video" collection
-    db.collection('video').insertMany(video, (err, result) => {
-      if (err) {
+    return db.collection('video').insertMany(video)
+      .then(result => {
+        console.log(`${result.insertedCount} documents were inserted into the 'video' collection`);
+      })
+      .catch(err => {
         console.error(err);
-        return;
-      }
-  
-      console.log(`${result.insertedCount} documents were inserted into the 'video' collection`);
-    });
-  
+      });
+  })
+  .then(() => {
     // Close the connection to the MongoDB server
     client.close();
+    console.log('Script finished');
+  })
+  .catch(err => {
+    console.error(err);
   });
-
-  console.log('Script finished');
