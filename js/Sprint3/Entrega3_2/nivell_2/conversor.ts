cffa79fb9c42f0b1568c71dba6e15232cc050ef8
JulenJR <julenjr@hotmail.com>
2023-04-13 12:49:22 +0200

    added more converter values to test the app

--- js/Sprint3/Entrega3_2/nivell_2/conversor.ts
+++ js/Sprint3/Entrega3_2/nivell_2/conversor.ts
@@ -15,11 +15,19 @@ function currencyConversion(target: any, key: string, descriptor: PropertyDescri
 class CurrencyConverter {
   @currencyConversion
   convertToEuros(currency: string, value: number) {
-    console.log(`Converted ${currency[1]} ${currency[0].slice(0,3)} to ${value} EUR`);
+    let roundValue = Math.round(value *100) / 100;
+    console.log(`Converted ${currency[1]} ${currency[0].slice(0,3)} to ${roundValue} EUR`);
   }
 }
 
 const converter1 = new CurrencyConverter();
 converter1.convertToEuros("USD_EUR", 100);
+
 const converter2 = new CurrencyConverter();
-converter2.convertToEuros("GBP_EUR", 100);
+converter2.convertToEuros("GBP_EUR", 75);
+
+const converter3 = new CurrencyConverter();
+converter3.convertToEuros("CHF_EUR", 50);
+
+const converter4 = new CurrencyConverter();
+converter4.convertToEuros("JPY_EUR",2500);
