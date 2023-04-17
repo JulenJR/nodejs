5b4b5f542218fc024e25ff7af2f316c9431b82a7
JulenJR <julenjr@hotmail.com>
2023-04-17 15:51:32 +0200

    rmq e_2 n_3

---
+++ js/Sprint3/Entrega3_2/nivell_3/app.ts
@@ -0,0 +1,18 @@
+import { Publisher, Subscriber } from './rabbitmq';
+
+async function main() {
+  const queue = 'my-queue';
+  const url = 'amqp://localhost';
+
+  const publisher = new Publisher(queue, url);
+  const subscriber = new Subscriber(queue, url);
+
+  await subscriber.subscribe();
+
+  setInterval(async () => {
+    const message = `Hello World! (${new Date().toISOString()})`;
+    await publisher.publish(message);
+  }, 1000);
+}
+
+main();
---
+++ js/Sprint3/Entrega3_2/nivell_3/rabbitmq.ts
@@ -0,0 +1,42 @@
+import amqp from 'amqplib';
+
+export class Publisher {
+  private readonly queue: string;
+  private readonly url: string;
+
+  constructor(queue: string, url: string) {
+    this.queue = queue;
+    this.url = url;
+  }
+
+  async publish(message: string): Promise<void> {
+    const connection = await amqp.connect(this.url);
+    const channel = await connection.createChannel();
+    await channel.assertQueue(this.queue);
+    channel.sendToQueue(this.queue, Buffer.from(message));
+    console.log(`Sent message "${message}" to queue "${this.queue}"`);
+    await channel.close();
+    await connection.close();
+  }
+}
+
+export class Subscriber {
+  private readonly queue: string;
+  private readonly url: string;
+
+  constructor(queue: string, url: string) {
+    this.queue = queue;
+    this.url = url;
+  }
+
+  async subscribe(): Promise<void> {
+    const connection = await amqp.connect(this.url);
+    const channel = await connection.createChannel();
+    await channel.assertQueue(this.queue);
+    console.log(`Waiting for messages in queue "${this.queue}"...`);
+    channel.consume(this.queue, (message) => {
+      console.log(`Received message "${message.content.toString()}" from queue "${this.queue}"`);
+      channel.ack(message);
+    });
+  }
+}
