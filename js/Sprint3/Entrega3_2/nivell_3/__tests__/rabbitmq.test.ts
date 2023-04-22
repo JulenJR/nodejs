c87d33bb2250cfecd5db03d820512d2b674b19d7
JulenJR <julenjr@hotmail.com>
2023-04-22 21:39:17 +0200

    test update with calls & server close

--- js/Sprint3/Entrega3_2/nivell_3/__tests__/rabbitmq.test.ts
+++ js/Sprint3/Entrega3_2/nivell_3/__tests__/rabbitmq.test.ts
@@ -10,6 +10,7 @@ describe('Publisher', () => {
   const url = 'amqp://localhost';
 
   test('publish should send message to queue and close connection', async () => {
+
     const mockConnection = { createChannel: jest.fn(), close: jest.fn() };
     const mockChannel = { assertQueue: jest.fn(), sendToQueue: jest.fn(), close: jest.fn() };
     (amqp.connect as jest.Mock).mockResolvedValue(mockConnection);
@@ -35,6 +36,7 @@ describe('Subscriber', () => {
   const url = 'amqp://localhost';
 
   test('subscribe should consume messages and acknowledge them', async () => {
+    
     const mockConnection = { createChannel: jest.fn(), close: jest.fn() };
     const mockChannel = { assertQueue: jest.fn(), consume: jest.fn(), ack: jest.fn() };
     (amqp.connect as jest.Mock).mockResolvedValue(mockConnection);
@@ -47,10 +49,14 @@ describe('Subscriber', () => {
     expect(mockConnection.createChannel).toHaveBeenCalled();
     expect(mockChannel.assertQueue).toHaveBeenCalledWith(queue);
     expect(mockChannel.consume).toHaveBeenCalledWith(queue, expect.any(Function));
+
     mockChannel.consume.mock.calls[0][1](null);
+
     expect(mockChannel.ack).not.toHaveBeenCalled();
+
     const testMessage = { content: { toString: () => 'test message' } };
     mockChannel.consume.mock.calls[0][1](testMessage);
+
     expect(mockChannel.ack).toHaveBeenCalledWith(testMessage);
     expect(mockConnection.close).not.toHaveBeenCalled();
   });
