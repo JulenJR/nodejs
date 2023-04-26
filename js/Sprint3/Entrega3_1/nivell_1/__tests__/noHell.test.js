c60128d898e6c348fa5f903bdf43c2eab54dbf7a
JulenJR <julenjr@hotmail.com>
2023-04-26 10:45:43 +0200

    e_ n_1 updated

--- js/Sprint3/Entrega3_1/nivell_1/__tests__/noHell.test.js
+++ js/Sprint3/Entrega3_1/nivell_1/__tests__/noHell.test.js
@@ -17,18 +17,3 @@ test("reverseText should reverse a string correctly", () => {
 });
 
 // Test for handling inaccessible folder error
-test('Error: Folder inaccessible', () => {
-  const originalReaddir = fs.readdir;
-
-  fs.readdir = (dir, cb) => {
-    const error = new Error('Folder inaccessible');
-    cb(error, null);
-  };
-
-  reverseText('hello', (error) => {
-    expect(error).toBeTruthy();
-    expect(error.message).toBe('Error: Folder inaccessible');
-
-    fs.readdir = originalReaddir;
-  });
-});
--- js/Sprint3/Entrega3_1/nivell_1/app/noHell.js
+++ js/Sprint3/Entrega3_1/nivell_1/app/noHell.js
@@ -3,8 +3,8 @@ const { error } = require("console");
 const { readdirSync, readFileSync, writeFileSync } = require("fs");
 const { join } = require("path");
 
-const inbox = join(__dirname, "inbox");
-const outbox = join(__dirname, "outbox");
+const inbox = join(__dirname, "../inbox");
+const outbox = join(__dirname, "../outbox");
 
 const reverseText = (str) => str.split("").reverse().join("");
 
@@ -21,18 +21,24 @@ function llegirDir(inbox) {
 }
 
 function noHell() {
-  const fitxers = llegirDir(inbox);
+  try {
+    
+    const fitxers = llegirDir(inbox);
+    if ( !fitxers.length ) throw new Error('the folder is empty')
 
-  fitxers.forEach((fitxer) => {
-    try {
+    fitxers.forEach((fitxer) => {
+
       const chain = llegirFitxer(fitxer);
-      escriureFitxer(fitxer, chain);
-      console.log(`${fitxer} was succesfully saved in outbox`);
       
-    } catch (error) {
-      throw new Error (error);
-    }
-  });
+      chain && escriureFitxer(fitxer, chain);
+    })
+
+  } catch ({ message }) {
+
+    throw new Error('MESSAGE ERROR -> ' + message)
+
+  }
+
 }
 
 noHell();
